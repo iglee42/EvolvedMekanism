a5b2b1658cf80abe05a948818e686318b9d3a300
iglée42 <igleev2@gmail.com>
2025-04-08 18:14:37 +0200

    Add langs

--- src/main/java/fr/iglee42/evolvedmekanism/EvolvedMekanismLang.java
+++ src/main/java/fr/iglee42/evolvedmekanism/EvolvedMekanismLang.java
@@ -2,13 +2,21 @@ package fr.iglee42.evolvedmekanism;
 
 import mekanism.api.text.ILangEntry;
 import mekanism.common.Mekanism;
+import mekanism.common.MekanismLang;
 import net.minecraft.Util;
 
 public enum EvolvedMekanismLang implements ILangEntry {
 
+    // <type>.evolvedmekanism.<path>
+
     MEKANISM_EVOLVED("constants","mod_name"),
-    TIERED_STORAGE_CAPACITY("tiered_storage","capacity")
+    TIERED_STORAGE_CAPACITY("tiered_storage","capacity"),
+
     ;
+
+    // <type>.mekanism.<path>
+    public static MekanismLang ALLOYING; // type : factory | path : alloying
+    public static MekanismLang DESCRIPTION_ALLOYER; // type : description | path : alloyer
 
 
     private final String key;
