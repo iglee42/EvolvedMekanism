fe4839a986db83aca69e856da0cbae4e01ee8ebf
iglée42 <igleev2@gmail.com>
2025-10-13 22:25:25 +0200

    Sort items

--- src/main/java/fr/iglee42/evolvedmekanism/registries/EMItems.java
+++ src/main/java/fr/iglee42/evolvedmekanism/registries/EMItems.java
@@ -27,13 +27,49 @@ import java.util.Locale;
 public class EMItems {
     public static final ItemDeferredRegister ITEMS = new ItemDeferredRegister(EvolvedMekanism.MODID);
 
-    //MODULES
+    public static final ItemRegistryObject<ItemAlloy> HYPERCHARGED_ALLOY = registerAlloy(EMAlloyTier.HYPERCHARGED);
+    public static final ItemRegistryObject<ItemAlloy> SUBATOMIC_ALLOY = registerAlloy(EMAlloyTier.SUBATOMIC);
+    public static final ItemRegistryObject<ItemAlloy> SINGULAR_ALLOY = registerAlloy(EMAlloyTier.SINGULAR);
+    public static final ItemRegistryObject<ItemAlloy> EXOVERSAL_ALLOY = registerAlloy(EMAlloyTier.EXOVERSAL);
+    public static final ItemRegistryObject<ItemAlloy> CREATIVE_ALLOY = registerAlloy(EMAlloyTier.CREATIVE);
+
+    public static final ItemRegistryObject<Item> OVERCLOCKED_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.OVERCLOCKED);
+    public static final ItemRegistryObject<Item> QUANTUM_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.QUANTUM);
+    public static final ItemRegistryObject<Item> DENSE_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.DENSE);
+    public static final ItemRegistryObject<Item> MULTIVERSAL_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.MULTIVERSAL);
+    public static final ItemRegistryObject<Item> CREATIVE_CONTROL_CIRCUIT = registerCircuit(BaseTier.CREATIVE);
+
+    public static final ItemRegistryObject<ItemTierInstaller> OVERCLOCKED_TIER_INSTALLER = registerInstaller(BaseTier.ULTIMATE, EMBaseTier.OVERCLOCKED);
+    public static final ItemRegistryObject<ItemTierInstaller> QUANTUM_TIER_INSTALLER = registerInstaller(EMBaseTier.OVERCLOCKED, EMBaseTier.QUANTUM);
+    public static final ItemRegistryObject<ItemTierInstaller> DENSE_TIER_INSTALLER = registerInstaller(EMBaseTier.QUANTUM,EMBaseTier.DENSE);
+    public static final ItemRegistryObject<ItemTierInstaller> MULTIVERSAL_TIER_INSTALLER = registerInstaller(EMBaseTier.DENSE,EMBaseTier.MULTIVERSAL);
+    public static final ItemRegistryObject<ItemTierInstaller> CREATIVE_TIER_INSTALLER = registerInstaller(EMBaseTier.MULTIVERSAL,BaseTier.CREATIVE);
+
+    public static final ItemRegistryObject<ItemQIODrive> BOOSTED_QIO_DRIVE = registerQIODrive(EMQIODriveTier.BOOSTED);
+    public static final ItemRegistryObject<ItemQIODrive> SINGULARITY_QIO_DRIVE = registerQIODrive(EMQIODriveTier.SINGULARITY);
+    public static final ItemRegistryObject<ItemQIODrive> HYPRA_SOLIDIFIED_QIO_DRIVE = registerQIODrive(EMQIODriveTier.HYPRA_SOLIDIFIED);
+    public static final ItemRegistryObject<ItemQIODrive> BLACK_HOLE_QIO_DRIVE = registerQIODrive(EMQIODriveTier.BLACK_HOLE);
+    public static final ItemRegistryObject<ItemQIODrive> CREATIVE_QIO_DRIVE = registerQIODrive(EMQIODriveTier.CREATIVE);
+
+    public static final ItemRegistryObject<Item> BETTER_GOLD_INGOT = registerResource(ResourceType.INGOT, EMResources.BETTER_GOLD);
+    public static final ItemRegistryObject<Item> PLASLITHERITE_INGOT = registerUnburnableResource(ResourceType.INGOT, EMResources.PLASLITHERITE);
+    public static final ItemRegistryObject<Item> REFINED_REDSTONE_INGOT = registerUnburnableResource(ResourceType.INGOT, EMResources.REFINED_REDSTONE);
+
+    public static final ItemRegistryObject<Item> BETTER_GOLD_NUGGET = registerResource(ResourceType.NUGGET, EMResources.BETTER_GOLD);
+    public static final ItemRegistryObject<Item> PLASLITHERITE_NUGGET = registerUnburnableResource(ResourceType.NUGGET, EMResources.PLASLITHERITE);
+    public static final ItemRegistryObject<Item> REFINED_REDSTONE_NUGGET = registerUnburnableResource(ResourceType.NUGGET, EMResources.REFINED_REDSTONE);
+
+    public static final ItemRegistryObject<Item> BETTER_GOLD_DUST = registerResource(ResourceType.DUST, EMResources.BETTER_GOLD);
+    public static final ItemRegistryObject<Item> PLASLITHERITE_DUST = registerUnburnableResource(ResourceType.DUST, EMResources.PLASLITHERITE);
+
+    public static final ItemRegistryObject<Item> ENRICHED_URANIUM = registerResource(ResourceType.ENRICHED, PrimaryResource.URANIUM);
+    public static final ItemRegistryObject<Item> ENRICHED_BETTER_GOLD = registerResource(ResourceType.ENRICHED,EMResources.BETTER_GOLD);
+    public static final ItemRegistryObject<Item> ENRICHED_PLASLITHERITE = registerUnburnableResource(ResourceType.ENRICHED,EMResources.PLASLITHERITE);
+
     public static final ItemRegistryObject<ItemModule> AIR_AFFINITY = ITEMS.registerModule(EMModules.AIR_AFFINITY, Rarity.RARE);
     public static final ItemRegistryObject<ItemModule> AQUA_AFFINITY = ITEMS.registerModule(EMModules.AQUA_AFFINITY,Rarity.RARE);
     public static final ItemRegistryObject<ItemModule> CAPTURING = ITEMS.registerModule(EMModules.CAPTURING,Rarity.RARE);
     public static final ItemRegistryObject<ItemModule> LUCK = ITEMS.registerModule(EMModules.LUCK,Rarity.RARE);
-
-    public static final ItemRegistryObject<ItemUpgrade> RADIOACTIVE_UPGRADE = registerUpgrade(EMUpgrades.RADIOACTIVE_UPGRADE);
 
     public static final ItemRegistryObject<Item> MOLD_BLOCK = registerMold("storage_block");
     public static final ItemRegistryObject<Item> MOLD_INGOT = registerMold("ingot");
@@ -46,46 +82,7 @@ public class EMItems {
     public static final ItemRegistryObject<Item> MOLD_WIRE = registerMold("wire");
     public static final ItemRegistryObject<Item> MOLD_COIN = registerMold("coin");
 
-
-    public static final ItemRegistryObject<ItemTierInstaller> OVERCLOCKED_TIER_INSTALLER = registerInstaller(BaseTier.ULTIMATE, EMBaseTier.OVERCLOCKED);
-    public static final ItemRegistryObject<ItemTierInstaller> QUANTUM_TIER_INSTALLER = registerInstaller(EMBaseTier.OVERCLOCKED, EMBaseTier.QUANTUM);
-    public static final ItemRegistryObject<ItemTierInstaller> DENSE_TIER_INSTALLER = registerInstaller(EMBaseTier.QUANTUM,EMBaseTier.DENSE);
-    public static final ItemRegistryObject<ItemTierInstaller> MULTIVERSAL_TIER_INSTALLER = registerInstaller(EMBaseTier.DENSE,EMBaseTier.MULTIVERSAL);
-    public static final ItemRegistryObject<ItemTierInstaller> CREATIVE_TIER_INSTALLER = registerInstaller(EMBaseTier.MULTIVERSAL,BaseTier.CREATIVE);
-
-    public static final ItemRegistryObject<Item> OVERCLOCKED_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.OVERCLOCKED);
-    public static final ItemRegistryObject<Item> QUANTUM_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.QUANTUM);
-    public static final ItemRegistryObject<Item> DENSE_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.DENSE);
-    public static final ItemRegistryObject<Item> MULTIVERSAL_CONTROL_CIRCUIT = registerCircuit(EMBaseTier.MULTIVERSAL);
-    public static final ItemRegistryObject<Item> CREATIVE_CONTROL_CIRCUIT = registerCircuit(BaseTier.CREATIVE);
-
-    public static final ItemRegistryObject<ItemQIODrive> BOOSTED_QIO_DRIVE = registerQIODrive(EMQIODriveTier.BOOSTED);
-    public static final ItemRegistryObject<ItemQIODrive> SINGULARITY_QIO_DRIVE = registerQIODrive(EMQIODriveTier.SINGULARITY);
-    public static final ItemRegistryObject<ItemQIODrive> HYPRA_SOLIDIFIED_QIO_DRIVE = registerQIODrive(EMQIODriveTier.HYPRA_SOLIDIFIED);
-    public static final ItemRegistryObject<ItemQIODrive> BLACK_HOLE_QIO_DRIVE = registerQIODrive(EMQIODriveTier.BLACK_HOLE);
-    public static final ItemRegistryObject<ItemQIODrive> CREATIVE_QIO_DRIVE = registerQIODrive(EMQIODriveTier.CREATIVE);
-
-    public static final ItemRegistryObject<ItemAlloy> HYPERCHARGED_ALLOY = registerAlloy(EMAlloyTier.HYPERCHARGED);
-    public static final ItemRegistryObject<ItemAlloy> SUBATOMIC_ALLOY = registerAlloy(EMAlloyTier.SUBATOMIC);
-    public static final ItemRegistryObject<ItemAlloy> SINGULAR_ALLOY = registerAlloy(EMAlloyTier.SINGULAR);
-    public static final ItemRegistryObject<ItemAlloy> EXOVERSAL_ALLOY = registerAlloy(EMAlloyTier.EXOVERSAL);
-    public static final ItemRegistryObject<ItemAlloy> CREATIVE_ALLOY = registerAlloy(EMAlloyTier.CREATIVE);
-
-    public static final ItemRegistryObject<Item> ENRICHED_URANIUM = registerResource(ResourceType.ENRICHED, PrimaryResource.URANIUM);
-    public static final ItemRegistryObject<Item> ENRICHED_BETTER_GOLD = registerResource(ResourceType.ENRICHED,EMResources.BETTER_GOLD);
-    public static final ItemRegistryObject<Item> ENRICHED_PLASLITHERITE = registerUnburnableResource(ResourceType.ENRICHED,EMResources.PLASLITHERITE);
-
-    public static final ItemRegistryObject<Item> BETTER_GOLD_DUST = registerResource(ResourceType.DUST, EMResources.BETTER_GOLD);
-    public static final ItemRegistryObject<Item> PLASLITHERITE_DUST = registerUnburnableResource(ResourceType.DUST, EMResources.PLASLITHERITE);
-
-    public static final ItemRegistryObject<Item> BETTER_GOLD_INGOT = registerResource(ResourceType.INGOT, EMResources.BETTER_GOLD);
-    public static final ItemRegistryObject<Item> PLASLITHERITE_INGOT = registerUnburnableResource(ResourceType.INGOT, EMResources.PLASLITHERITE);
-    public static final ItemRegistryObject<Item> REFINED_REDSTONE_INGOT = registerUnburnableResource(ResourceType.INGOT, EMResources.REFINED_REDSTONE);
-
-    public static final ItemRegistryObject<Item> BETTER_GOLD_NUGGET = registerResource(ResourceType.NUGGET, EMResources.BETTER_GOLD);
-    public static final ItemRegistryObject<Item> PLASLITHERITE_NUGGET = registerUnburnableResource(ResourceType.NUGGET, EMResources.PLASLITHERITE);
-    public static final ItemRegistryObject<Item> REFINED_REDSTONE_NUGGET = registerUnburnableResource(ResourceType.NUGGET, EMResources.REFINED_REDSTONE);
-
+    public static final ItemRegistryObject<ItemUpgrade> RADIOACTIVE_UPGRADE = registerUpgrade(EMUpgrades.RADIOACTIVE_UPGRADE);
 
     private static ItemRegistryObject<Item> registerCircuit(BaseTier tier) {
         return ITEMS.registerItem(tier.getLowerName() + "_control_circuit", properties -> new Item(properties) {
