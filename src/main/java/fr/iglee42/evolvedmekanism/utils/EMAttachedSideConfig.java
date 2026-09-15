b98f79eae71439c1bc017064a3b6b818df45741a
woxayz <woxayz@gmail.com>
2026-09-15 20:13:32 +0200

    Game crashes with IndexOutOfBoundsException whenever opening the inventory

--- src/main/java/fr/iglee42/evolvedmekanism/utils/EMAttachedSideConfig.java
+++ src/main/java/fr/iglee42/evolvedmekanism/utils/EMAttachedSideConfig.java
@@ -4,13 +4,19 @@ import mekanism.api.RelativeSide;
 import mekanism.common.attachments.component.AttachedSideConfig;
 import mekanism.common.lib.transmitter.TransmissionType;
 import mekanism.common.tile.component.config.DataType;
-import mekanism.common.util.EnumUtils;
 import net.minecraft.Util;
 
 import java.util.EnumMap;
 import java.util.Map;
 
 public class EMAttachedSideConfig {
+
+    private static final AttachedSideConfig.LightConfigInfo TWO_INPUT = Util.make(() -> {
+        Map<RelativeSide, DataType> sideConfig = new EnumMap<>(RelativeSide.class);
+        sideConfig.put(RelativeSide.LEFT, DataType.INPUT_1);
+        sideConfig.put(RelativeSide.RIGHT, DataType.INPUT_2);
+        return new AttachedSideConfig.LightConfigInfo(sideConfig, false);
+    });
     public static final AttachedSideConfig CHEMIXER_MACHINE = Util.make(() -> {
         Map<TransmissionType, AttachedSideConfig.LightConfigInfo> configInfo = new EnumMap<>(TransmissionType.class);
         configInfo.put(TransmissionType.ITEM, AttachedSideConfig.LightConfigInfo.EXTRA_MACHINE);
@@ -30,7 +36,7 @@ public class EMAttachedSideConfig {
     public static final AttachedSideConfig SOLIDIFICATION_MACHINE = Util.make(() -> {
         Map<TransmissionType, AttachedSideConfig.LightConfigInfo> configInfo = new EnumMap<>(TransmissionType.class);
         configInfo.put(TransmissionType.ITEM, AttachedSideConfig.LightConfigInfo.MACHINE);
-        configInfo.put(TransmissionType.FLUID,AttachedSideConfig.LightConfigInfo.TWO_INPUT_AND_OUT);
+        configInfo.put(TransmissionType.FLUID, TWO_INPUT);
         configInfo.put(TransmissionType.ENERGY, AttachedSideConfig.LightConfigInfo.INPUT_ONLY);
         return new AttachedSideConfig(configInfo);
     });
